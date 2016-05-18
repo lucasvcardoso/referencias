76209cee76c74612f82052c21831de680a60fa67
lucasvcardoso <lucas.v.cardoso@hotmail.com>
2016-05-18 13:59:02 -0300

    Atualização da classe Logger para utilizar enum ao invés de repetição de código

--- C# classes/Logger.cs
+++ C# classes/Logger.cs
@@ -5,32 +5,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NamespaceName
+namespace DomainScannerFileMerger
 {
     static class Logger
     {
         private static StreamWriter _writer;
-        public static void LogError(string errorMessage)
+        public static void Log(string logMessage, string logType)
         {
             using (_writer = new StreamWriter(@".\FileMerger.log", append: true))
             {
-                _writer.WriteLine(string.Format("{0}[ERROR] - {1}", DateTime.Now.ToString("dd/MM/yyyy HH:MM:ss"), errorMessage));
+                _writer.WriteLine(string.Format("{0}[{1}] - {2}", DateTime.Now.ToString("dd/MM/yyyy HH:MM:ss"), logType, logMessage));
             }
         }
-
-        public static void LogWarning(string warningMessage)
-        {
-            using (_writer = new StreamWriter(@".\FileMerger.log", append: true))
-            {
-                _writer.WriteLine(string.Format("{0}[WARNING] - {1}", DateTime.Now.ToString("dd/MM/yyyy HH:MM:ss"), warningMessage));
-            }
-        }
-
-        public static void LogInfo(string infoMessage)
-        {
-            using (_writer = new StreamWriter(@".\FileMerger.log", append: true))            {
-                _writer.WriteLine(string.Format("{0}[INFO] - {1}", DateTime.Now.ToString("dd/MM/yyyy HH:MM:ss"), infoMessage));
-            }
-        }
+        
+        public enum LogType { ERROR, INFO, WARNING };
     }
 }
