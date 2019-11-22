4ec1bf71ce1f2f3abca76ae1bc493f7424cf92cf
Lucas V. Cardoso <lucas.v.cardoso@gmail.com>
2019-11-22 11:31:21 -0300

    Added script to uninstall win services

---
+++ UninstallWinService.ps1
@@ -0,0 +1,13 @@
+﻿Param(
+    [string]$ServiceName
+)
+
+$Service = Get-WmiObject -Class Win32_Service -Filter "Name = '$ServiceName'"
+
+if ($Service -ne $null)
+{
+    $Service | stop-service -Force
+    Start-Sleep -s 10
+    $Service.Delete()
+}
+
