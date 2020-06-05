4bb83a01d07688dfb7cf5834a60e478d7107ff21
Lucas Cardoso <lucas.v.cardoso@gmail.com>
2020-06-05 17:41:01 -0300

    Adding set process priority PS1 and AWSS3 signing example

---
+++ C# classes/AwsGeneratePreSignedS3Url/AwsGeneratePreSignedS3Url/GenPresignedURLTest.cs
@@ -0,0 +1,52 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+
+namespace S3Sample
+{
+    class GenPresignedURLTest
+    {
+        private const string bucketName = "kbcprivate";
+        private const string objectKey = "00_PUBLIC/Degreed/EL0866_1on1_Wrksht.pdf";//"00_PUBLIC/Degreed/EL0866_1on1_Wrksht.pdf";
+        // Specify your bucket region (an example region is shown).
+        private static readonly RegionEndpoint bucketRegion = Amazon.RegionEndpoint.GetBySystemName("kbcprivate");
+        private static IAmazonS3 s3Client;
+        private static string _awsSecretKey = "BAypaFYAvKz4h57FNkCa2JqejI+nPtxwL603y1ZF";//"BAypaFYAvKz4h57FNkCa2JqejI+nPtxwL603y1ZF";
+        private static string _awsAccessKey = "AKIA2B4R2X6X6IDS2PUB";//"AKIA2B4R2X6X6IDS2PUB";
+
+        public static void Main()
+        {
+            var creds = new BasicAWSCredentials(_awsAccessKey, _awsSecretKey);
+            s3Client = new AmazonS3Client(creds, bucketRegion);
+            string urlString = GeneratePreSignedURL();
+            Console.WriteLine(urlString);
+            Console.ReadLine();
+        }
+        static string GeneratePreSignedURL()
+        {
+            string urlString = "";
+            try
+            {
+                GetPreSignedUrlRequest request1 = new GetPreSignedUrlRequest
+                {
+                    BucketName = bucketName,
+                    Key = objectKey,
+                    Expires = DateTime.Now.AddMinutes(5),
+                    Verb = HttpVerb.GET
+                };
+                urlString = s3Client.GetPreSignedURL(request1);
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine("Error encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
+            return urlString;
+        }
+    }
+}
--- SetProcessPriority.ps1
+++ SetProcessPriority.ps1
@@ -1,3 +1,6 @@
 ﻿ Get-WmiObject Win32_process -filter 'name = "MSBuild.exe"' | foreach-object { $_.SetPriority(256) }
  Get-WmiObject Win32_process -filter 'name = "devenv.exe"' | foreach-object { $_.SetPriority(256) }
  Get-WmiObject Win32_process -filter 'name = "node.exe"' | foreach-object { $_.SetPriority(256) }
+ Get-WmiObject Win32_process -filter 'name = "firefox.exe"' | foreach-object { $_.SetPriority(256) }
+ Get-WmiObject Win32_process -filter 'name = "iisexpress.exe"' | foreach-object { $_.SetPriority(256) }
+ Get-WmiObject Win32_process -filter 'name = "iisexpresstray.exe"' | foreach-object { $_.SetPriority(256) }
