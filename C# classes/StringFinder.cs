2caf612db70979224e1be43b0a528c88ae5bab78
Lucas Cardoso <lucas.v.cardoso@gmail.com>
2020-08-18 20:55:56 -0300

    Finally adding string finder

---
+++ C# classes/StringFinder.cs
@@ -0,0 +1,57 @@
+public class StringFinder
+{
+    static List<string> _dbItems = new List<string> { items };
+    static void Main(string[] args)
+    {
+        try
+        {
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //foreach (string d in Directory.GetFiles(@directory, pattern, SearchOption.AllDirectories))
+            //{
+            //    //Console.WriteLine();
+            //    SearchStrings(d);
+            //}
+            //Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (string d in Directory.GetFiles(@, "FILE_NAME_HERE" , SearchOption.AllDirectories))
+            {
+                //Console.WriteLine();
+                SearchStrings(d);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+
+    private static void SearchStrings(string d)
+    {
+        List<string> list = new List<string>();
+        using (StreamReader reader = new StreamReader(d))
+        {
+            int lineNumber = 1;
+            while (reader.Peek() >= 0)
+            {
+                var line = reader.ReadLine();
+                foreach (string dbItem in _dbItems)
+                {
+                    if (line.ToUpper().IndexOf(dbItem.ToUpper()) > -1)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(d);
+                        Console.WriteLine(line);
+                        Console.WriteLine();
+                    }
+                }
+                lineNumber++;
+            }
+        }
+    }
+}
